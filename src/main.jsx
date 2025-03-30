ddbbb5cb55d9f02d7b7c4f94b19be53aac1d7afa
jb <johnbenedictaparicio@gmail.com>
2025-03-31 02:40:48 +0800

    fixed accordion drop down, temp candidate list

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import Home from './Home.jsx'
+// Fixes the accordions not collapsing 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 // ROUTING
 import { BrowserRouter } from "react-router-dom";
 
