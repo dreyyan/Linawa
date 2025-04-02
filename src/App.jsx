6454df273f4dd1bd310602f324771903f21fe05b
hiimkhel <kellyydrhan.alojepan@wvsu.edu.ph>
2025-04-03 00:58:59 +0800

    Added private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// MODULES
-import Header from './components/Header.jsx'
-import Grid from './components/Grid.jsx';
-import CandidatesProfile from './components/CandidatesProfile.jsx'
-import CandidateDetail from './components/CandidateDetail.jsx';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header.jsx';
+import CandidatesProfile from './components/CandidatesProfile.jsx';
 // PAGES
 import Candidates from './pages/candidates.jsx';
 import Policies from './pages/policies.jsx';
@@ -13,7 +10,7 @@ import News from './pages/news.jsx';
 import AboutUs from './pages/about-us.jsx';
 // BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.css';
-import "./App.css";
+import './App.css';
 // Import the PrivateRoutes component
 import PrivateRoutes from './components/PrivateRoutes.jsx';
 import LoginPage from './pages/LoginPage.jsx';
@@ -22,20 +19,69 @@ import SignUpPage from './pages/SignupPage.jsx';
 function Home() {
   return (
     <>
-        <Header/>
-      <div className="routes">
-          <Routes>
-                      {/* Redirect to LoginPage by default */}
-          <Route path="/" element={<LoginPage />} />
-            <Route path="/pages/candidates" element={<Candidates />} />
-            <Route path="/candidates/:id" element={<CandidatesProfile />} />
-            <Route path="/pages/candidate-profiles/:name" element={<CandidateDetail />} />
-            <Route path="/pages/policies" element={<Policies />} />
-            <Route path="/pages/petitions-and-polls" element={<PetitionsAndPolls />} />
-            <Route path="/pages/news" element={<News />} />
-            <Route path="/pages/about-us" element={<AboutUs />} />
-          </Routes>
-      </div>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        
+        {/* Private Routes */}
+        <Route element={<PrivateRoutes />}>
+          <Route
+            path="/pages/candidates"
+            element={
+              <>
+                <Header /> 
+                <Candidates />
+              </>
+            }
+          />
+          <Route
+            path="/candidates/:id"
+            element={
+              <>
+                <Header /> 
+                <CandidatesProfile />
+              </>
+            }
+          />
+          <Route
+            path="/pages/policies"
+            element={
+              <>
+                <Header /> 
+                <Policies />
+              </>
+            }
+          />
+          <Route
+            path="/pages/petitions-and-polls"
+            element={
+              <>
+                <Header /> 
+                <PetitionsAndPolls />
+              </>
+            }
+          />
+          <Route
+            path="/pages/news"
+            element={
+              <>
+                <Header /> 
+                <News />
+              </>
+            }
+          />
+          <Route
+            path="/pages/about-us"
+            element={
+              <>
+                <Header /> 
+                <AboutUs />
+              </>
+            }
+          />
+        </Route>
+      </Routes>
     </>
   );
 }
